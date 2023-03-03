726fed7ab1008321c768b9c1cbf67d03008659dc
wfly <953212389@qq.com>
2023-03-03 15:49:27 +0800

    1

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+export {};
+
+declare global {
+	type Nullable<T> = T | null;
+	type Recordable<T = any> = Record<string, T>;
+	type Numberish = number | string;
+
+	type TargetContext = '_self' | '_blank';
+	
+	type ModulesDefaultType = Record<string, {default: Record<string, any>}>;
+	
+	interface Fn<T = any, R = T> {
+		(...arg: T[]): R;
+	}
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'tsup';
+import { resolve } from 'path';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: {
-    resolve: true
+    resolve: true,
+    compilerOptions: {
+      "skipLibCheck": true,
+    }
   },
+  tsconfig: resolve('./tsconfig.json'),
   clean: true,
   sourcemap: true
 });
