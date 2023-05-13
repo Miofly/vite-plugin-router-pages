863acf7070a936324cf8384469aa096b7244d39d
wfly <953212389@qq.com>
2023-05-13 11:19:10 +0800

    feat: update

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 import { version } from '../package.json';
 
-let command = 'pnpm publish --access public --no-git-checks';
+let command = 'git commit -am \'feat: update\' && pnpm version patch && pnpm publish --access public --no-git-checks';
 
 if (version.includes('beta'))
   command += ' --tag beta';
