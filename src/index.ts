3db4f540c1392995dee370c4890b16d52bdd2f3d
wfly <953212389@qq.com>
2023-09-02 15:34:15 +0800

    feat: add new function and optimize code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,67 @@
 import type { Plugin } from 'vite';
-export function pagesPlugin(): Plugin {
+import {
+  MODULE_ID_VIRTUAL,
+  ROUTE_BLOCK_ID_VIRTUAL,
+  routeBlockQueryRE,
+} from './constants';
+import { PageContext } from './context';
+import type { UserOptions } from './types';
+
+import { parsePageRequest } from './utils';
+
+function pagesPlugin(userOptions: UserOptions = {}): Plugin {
+  let ctx: PageContext;
+
   return {
     name: 'vite-plugin-pages',
     enforce: 'pre',
-    async configResolved(config) {},
+    async configResolved(config) {
+      ctx = new PageContext(userOptions, config.root);
+      ctx.setLogger(config.logger);
+      await ctx.searchGlob();
+    },
+    api: {
+      getResolvedRoutes() {
+        return ctx.options.resolver.getComputedRoutes(ctx);
+      },
+    },
+    configureServer(server) {
+      ctx.setupViteServer(server);
+    },
+    resolveId(id) {
+      if (ctx.options.moduleIds.includes(id))
+        return `${MODULE_ID_VIRTUAL}?id=${id}`;
+
+      if (routeBlockQueryRE.test(id)) return ROUTE_BLOCK_ID_VIRTUAL;
+
+      return null;
+    },
+    async load(id) {
+      const { moduleId, pageId } = parsePageRequest(id);
+
+      if (
+        moduleId === MODULE_ID_VIRTUAL &&
+        pageId &&
+        ctx.options.moduleIds.includes(pageId)
+      )
+        return ctx.resolveRoutes();
+
+      if (id === ROUTE_BLOCK_ID_VIRTUAL) {
+        return {
+          code: 'export default {};',
+          map: null,
+        };
+      }
+
+      return null;
+    },
   };
 }
+
+export * from './types';
+export type { VueRoute } from './resolvers';
+
+export { vueResolver } from './resolvers';
+export { syncIndexResolver } from './options';
+export { PageContext };
+export default pagesPlugin;
