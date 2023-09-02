7513426e30ec06bcc920908ede162d1ff5233420
wfly <953212389@qq.com>
2023-09-02 16:20:25 +0800

    feat: add new function and optimize code

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: {
+    resolve: true,
+    compilerOptions: {
+      skipLibCheck: true,
+    },
+  },
+  skipNodeModulesBundle: true,
+  tsconfig: resolve('./tsconfig.json'),
+  clean: true,
+  sourcemap: false,
+});
